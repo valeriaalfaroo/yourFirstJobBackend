d2fbeca1a222a18a8003992324e143bde2e09e84
emacr <manuelcarranzaw@gmail.com>
2024-03-20 19:39:19 -0600

    se crean las carpetas y las clases de usuario

--- yourFirstJobBack/Logica/LogUsuario.cs
+++ yourFirstJobBack/Logica/LogUsuario.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace yourFirstJobBack.Entidad
+namespace yourFirstJobBack.Logica
 {
-    internal class Usuario
+    internal class LogUsuario
     {
     }
 }
