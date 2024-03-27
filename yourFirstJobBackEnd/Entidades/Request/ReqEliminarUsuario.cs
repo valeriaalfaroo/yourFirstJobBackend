234cdfc7301c56dce13d3504910b1a34d3622b0b
emacr <manuelcarranzaw@gmail.com>
2024-03-26 20:44:57 -0600

    se crea eliminar usuario y logica + api

---
+++ yourFirstJobBackEnd/Entidades/Request/ReqEliminarUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using yourFirstJobBackend.Entidades.entities;
+
+namespace yourFirstJobBackend.Entidades.Request
+{
+    public class ReqEliminarUsuario
+    {
+        public int idUsuario { get; set; }
+    }
+}
