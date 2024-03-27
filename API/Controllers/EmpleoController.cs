d4b6005d9a64237994cab8f48a1c1583ba747ece
Valeria Alfaro Quesada <valfaro2409@gmail.com>
2024-03-26 20:16:31 -0600

    API Post Oferta empleo

--- API/Controllers/EmpleoController.cs
+++ API/Controllers/EmpleoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using yourFirstJobBackend.Entidades.Request;
 using yourFirstJobBackend.Entidades.Response;
 using yourFirstJobBackend.Logica;
 namespace API.Controllers
@@ -18,6 +19,14 @@ namespace API.Controllers
             LogOfertaEmpleo logicaBackend = new LogOfertaEmpleo();
             return logicaBackend.obtenerTodosLosEmpleos(null);
         }
-        
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/empleo/ingresarEmpleo")]
+        public ResIngresarEmpleo ingresarEmpleo(ReqIngresarEmpleo req)
+        {
+            LogOfertaEmpleo logicaBackend = new LogOfertaEmpleo();
+            return logicaBackend.ingresarEmpleo(null);
+        }
+
     }
 } 
