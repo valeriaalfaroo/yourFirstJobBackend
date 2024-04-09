4f5257114722cf909eb5e08aed11e18325d489bc
NachoGoyenaga <Nacho.goyenaga@gmail.com>
2024-04-09 04:30:28 -0600

    Updates, Insert y delete
    Updates de experiencia, estudios y habilidades
    
    Insert Idioma
    
    Delete Idioma

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -22,10 +22,28 @@ namespace API.Controllers
             return logicaBackend.IngresarUsuario(req);
         }
 
+        //Insert idioma usuario
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/usuario/insertIdiomaUsuario")]
+        public ResIngresarIdiomaUsuario ingresarIdiomaUsuario(ReqIngresarIdiomaUsuario req)
+        {
+            if (req == null)
+            {
+                return new ResIngresarIdiomaUsuario
+                {
+                    resultado = false,
+                    listaDeErrores = new List<string> { "Request nulo" }
+                };
+            }
+
+            LogUsuario logica = new LogUsuario();
+            return logica.IngresarIdiomaUsuario(req);
+        }
+
         //Obtener un usuario
         [System.Web.Http.HttpPost]
         [System.Web.Http.Route("api/usuario/obtenerUsuario")]
-        public ResObtenerPerfilUsuario obtenerUsuario([FromBody] ReqObtenerUsuario req)
+        public ResObtenerPerfilUsuario obtenerUsuario(ReqObtenerUsuario req)
         {
             if (req == null)
             {
@@ -74,12 +92,29 @@ namespace API.Controllers
             return logicaBackend.eliminarUsuario(null);
         }
 
+        //Delete idioma usuario
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/usuario/eliminarIdiomaUsuario")]
+        public ResEliminarIdiomaUsuario deleteIdiomaUsuario(ReqEliminarIdiomaUsuario req)
+        {
+            if (req == null)
+            {
+                return new ResEliminarIdiomaUsuario
+                {
+                    resultado = false,
+                    listaDeErrores = new List<string> { "Request nulo" }
+                };
+            }
+
+            LogUsuario logica = new LogUsuario();
+            return logica.eliminarIdiomaUsuario(req);
+        }
 
 
         //Update usuario
         [System.Web.Http.HttpPost]
         [System.Web.Http.Route("api/usuario/actualizarUsuario")]
-        public ResUpdateUsuario actualizarUsuario([FromBody] ReqUpdateUsuario req)
+        public ResUpdateUsuario actualizarUsuario(ReqUpdateUsuario req)
         {
             if (req == null)
             {
@@ -97,7 +132,7 @@ namespace API.Controllers
         //Update idioma usuario
         [System.Web.Http.HttpPost]
         [System.Web.Http.Route("api/usuario/actualizarUsuarioIdioma")]
-        public ResUpdateUsuarioIdioma actualizarIdiomaUsuario([FromBody] List<ReqUpdateUsuarioIdioma> req)
+        public ResUpdateUsuarioIdioma actualizarIdiomaUsuario(List<ReqUpdateUsuarioIdioma> req)
         {
             if (req == null)
             {
@@ -110,6 +145,60 @@ namespace API.Controllers
 
             LogUsuario logica = new LogUsuario();
             return logica.updateIdiomasUsuario(req);
+        }
+
+        //Update habilidad usuario
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/usuario/actualizarUsuarioHabilidad")]
+        public ResUpdateUsuarioHabilidades actualizarHabilidad(List<ReqUpdateUsuarioHabilidades> req)
+        {
+            if (req == null)
+            {
+                return new ResUpdateUsuarioHabilidades
+                {
+                    resultado = false,
+                    listaDeErrores = new List<string> { "Request nulo" }
+                };
+            }
+
+            LogUsuario logica = new LogUsuario();
+            return logica.updateHabilidadesUsuario(req);
+        }
+
+        //Update estudio usuario
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/usuario/actualizarUsuarioEstudio")]
+        public ResUpdateUsuarioEstudios actualizarHabilidad(List<ReqUpdateUsuarioEstudios> req)
+        {
+            if (req == null)
+            {
+                return new ResUpdateUsuarioEstudios
+                {
+                    resultado = false,
+                    listaDeErrores = new List<string> { "Request nulo" }
+                };
+            }
+
+            LogUsuario logica = new LogUsuario();
+            return logica.updateEstudiosUsuario(req);
+        }
+
+        //Update experiencia usuario
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("api/usuario/actualizarUsuarioExperiencia")]
+        public ResUpdateUsuarioExperiencia actualizarHabilidad(List<ReqUpdateUsuarioExperiencia> req)
+        {
+            if (req == null)
+            {
+                return new ResUpdateUsuarioExperiencia
+                {
+                    resultado = false,
+                    listaDeErrores = new List<string> { "Request nulo" }
+                };
+            }
+
+            LogUsuario logica = new LogUsuario();
+            return logica.updateExperienciaUsuario(req);
         }
 
 
